f497f4f7daca35eb9533fcd428053c2ea76b4b18
shuvalov_pa <shuvalov_pa@ascon.ru>
2024-09-23 15:25:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Xml.Linq;
+
+Console.Write("Введите свое имя: ");
+var first_name = Console.ReadLine();
+
+Console.Write("Введите свою фамилию: ");
+var last_name = Console.ReadLine();
+
+Console.Write("Введите свое отчество: ");
+var patronym = Console.ReadLine();
+
+Console.WriteLine($"Привет, {last_name} {first_name} {patronym}");
+
+
+Console.Write("Введите первое число: ");
+var a = Console.ReadLine();
+
+Console.Write("Введите второе число: ");
+var b = Console.ReadLine();
+
+Console.Write("Введите арифметическую операцию: ");
+var c = Console.ReadLine();
+
+if (c == "+")
+    {
+    double d = Convert.ToDouble(a) + Convert.ToDouble(b);
+    Console.WriteLine($"результат: {d}");
+    }
+if (c == "-")
+{
+    double d = Convert.ToDouble(a) - Convert.ToDouble(b);
+    Console.WriteLine($"результат: {d}");
+}
+if (c == "*")
+{
+    double d = Convert.ToDouble(a) * Convert.ToDouble(b);
+    Console.WriteLine($"результат: {d}");
+}
+if (c == "/")
+{
+    double d = Convert.ToDouble(a) / Convert.ToDouble(b);
+    Console.WriteLine($"результат: {d}");
+}
